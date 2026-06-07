9e410cea985207a8b23b9df7c5f4876178b0f1f9
Azka Maryam <azkamaryam213@gmail.com>
2026-06-08 02:22:59 +0500

    Configure application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Result from "./pages/Result";
+import BuyCredit from "./pages/BuyCredit";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/result" element={<Result />} />
+      <Route path="/buy" element={<BuyCredit />} />
+    </Routes>
   );
 }
 
