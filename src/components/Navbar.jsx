243112939b539b347218e9a77b9e2bc35f099478
Azka Maryam <azkamaryam213@gmail.com>
2026-06-08 02:31:03 +0500

    Add navigation bar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,19 @@
+import { assets } from "../assets/assets";
+
 function Navbar() {
-  return <h1>Navbar</h1>;
+  return (
+    <div className="flex items-center justify-between py-4">
+      <img
+        src={assets.logo}
+        alt="Logo"
+        className="w-32 sm:w-44 cursor-pointer"
+      />
+
+      <button className="bg-black text-white px-8 py-2 rounded-full text-sm">
+        Login
+      </button>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,14 @@ import Footer from "../components/Footer";
 
 function Home() {
   return (
-    <>
+    <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen">
       <Navbar />
       <Header />
       <Description />
       <Steps />
       <Testimonials />
       <Footer />
-    </>
+    </div>
   );
 }
 
